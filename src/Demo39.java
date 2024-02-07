bb1e0ccb3bd8a08814d8538d7dfda6c6d5b27bf9
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-02-07 11:31:45 +0530

    Add Demo39.java

--- src/Demo39.java
+++ src/Demo39.java
@@ -1,3 +1,12 @@
 void main(){
 
+    int y = 6;
+    do {
+        for (int x = 0; x++ < 4; System.out.print(x == 4 ? "= " : "")) {
+            System.out.print("* ");
+            if (x == 4 && y % 2 != 0) break;
+        }
+        y--;
+        System.out.println();
+    }while(y != 0);
 }
